4862246fd8773e2dfaa79b6264e74eca9b417e2f
Joshua Aquino <email@joshuaaquino.xyz>
2022-04-11 16:53:10 -0400

    add pane2 entry traversal

--- ui/rangerEntry.cs
+++ ui/rangerEntry.cs
@@ -1,19 +1,19 @@
 using Godot;
 using System;
 
-public class rangerEntry : Label
+public class rangerEntry : Button
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    [Export]
-    private string entryName = "defaultName";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	[Export]
+	private string entryName = "defaultName";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        this.Set("text", entryName);
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.Set("text", entryName);
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
--- ui/sceneMaker.cs
+++ ui/sceneMaker.cs
@@ -8,36 +8,78 @@ public class sceneMaker : HBoxContainer
     // private int a = 2;
     // private string b = "text";
     private string FILEPATH = "./files";
-    private Node pane2;
+    private Node pane1, pane2;
     private PackedScene entryScene;
+    private Button newEntryScene;
     private string currFilePath;
     private string[] currEntries;
     private string[] prevEntries;
     private string[] nextEntries;
-    private string selectedEntry;
+    private int selectedEntry = 0;
+    
+    private void printPane(Node pane, string[] entries)
+    {
+        foreach (string entry in entries) 
+        {
+            newEntryScene = (Button)entryScene.Instance();
+            newEntryScene.Set("entryName", entry);
+            pane.AddChild(newEntryScene);
+        }
+    }
+    private void traverseEntry(byte direction) // 0123 hjkl
+    {
+        switch(direction) 
+        {
+            case 0 : //left h
+            break;
+            case 1 : //down j
+                if (selectedEntry < currEntries.GetUpperBound(0))
+                {
+                    selectedEntry++;
+                    pane2.GetChild(selectedEntry).Call("grab_focus");
+                }
+            break;
+            case 2 : //up k
+                if (selectedEntry > currEntries.GetLowerBound(0))
+                {
+                    selectedEntry--;
+                    pane2.GetChild(selectedEntry).Call("grab_focus");
+                }
+            break;
+            case 3 : //right l
+            break;
+        }
+    }
     
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        pane1 = this.GetChild(0).GetChild(0);
+        pane2 = this.GetChild(1).GetChild(0);
+        entryScene = (PackedScene)ResourceLoader.Load("res://ui/rangerEntry.tscn");
         currEntries = System.IO.Directory.GetFileSystemEntries(FILEPATH);
         prevEntries = currEntries;
+        
+        printPane(pane1, prevEntries);
+        printPane(pane2, currEntries);
+        
+        pane2.GetChild(selectedEntry).Call("grab_focus");
+
+        
         GD.Print(currEntries);
         GD.Print(prevEntries);
 
-        pane2 = this.GetChild(1).GetChild(0);
-        entryScene = (PackedScene)ResourceLoader.Load("res://ui/rangerEntry.tscn");
-        Label newEntryScene = (Label)entryScene.Instance();
-        pane2.AddChild(newEntryScene);
-        newEntryScene = (Label)entryScene.Instance();
-        pane2.AddChild(newEntryScene);
-        newEntryScene = (Label)entryScene.Instance();
-        pane2.AddChild(newEntryScene);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+    if(Input.IsActionJustPressed("next_entry")) {
+        traverseEntry(1);
+    }
+    if(Input.IsActionJustPressed("previous_entry")) {
+        traverseEntry(2);
+    }
+  }
 }
